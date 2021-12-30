1cddf0de812a7a6e8f446eb1bf1b6336fcfbc54e
babo24128 <81487189+babo24128@users.noreply.github.com>
2021-12-30 21:19:32 +0900

    second commit

--- Quad Action/Assets/Scripts/Player.cs
+++ Quad Action/Assets/Scripts/Player.cs
@@ -7,18 +7,26 @@ public class Player : MonoBehaviour
     //Input Axis 값을 받을 전역변수 선언
     float hAxis;
     float vAxis;
-
     bool wDown;
+    bool jDown;
     public float speed;
 
-    Vector3 moveVec;
+    bool isJump;
+    bool isDodge;
+  
 
+    Vector3 moveVec;
+    Vector3 dodgeVec;
+
+    Rigidbody rigid;
     Animator anim;
 
     private void Awake()
     {
+        rigid = GetComponent<Rigidbody>();
         //Animator 변수를 GetComponentInChildren()으로 초기화
         anim = GetComponentInChildren<Animator>();
+
     }
 
     // Start is called before the first frame update
@@ -30,13 +38,30 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        GetInput();
+        Move();
+        Turn();
+        Jump();
+        Dodge();
+    }
+
+    void GetInput()
+    {
         //Axis 값을 정수로 반환하는 함수
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
         //Shift는 누를 때만 작동하도록 GetButton() 함수 사용
         wDown = Input.GetButton("Walk");
+        jDown = Input.GetButton("Jump");
 
+    }
+
+    void Move()
+    {
         moveVec = new Vector3(hAxis, 0, vAxis).normalized;
+
+        if (isDodge)
+            moveVec = dodgeVec;
 
         if (wDown)
             transform.position += moveVec * speed * 0.3f * Time.deltaTime;
@@ -52,8 +77,57 @@ public class Player : MonoBehaviour
         anim.SetBool("isRun", moveVec != Vector3.zero);
         anim.SetBool("isWalk", wDown);
 
+    }
+
+    void Turn()
+    {
         //LookAt() : 지정된 벡터를 향해서 회전시켜주는 함수
         transform.LookAt(transform.position + moveVec);
     }
 
+    void Jump()
+    {
+        if (jDown && moveVec == Vector3.zero && !isJump && !isDodge)
+        {
+            // AddForce() 함수로 물리적인 힘 가하기
+            rigid.AddForce(Vector3.up * 15, ForceMode.Impulse);
+            anim.SetBool("isJump",true);
+            anim.SetTrigger("doJump");
+            isJump = true;
+        }
+    }
+
+    void Dodge()
+    {
+        if (jDown && moveVec != Vector3.zero && !isJump && !isDodge)
+        {
+            dodgeVec = moveVec;
+            // AddForce() 함수로 물리적인 힘 가하기
+            speed *= 2;
+            anim.SetTrigger("doDodge");
+            isDodge = true;
+
+            // Invoke() 함수로 시간차 함수 호출
+            Invoke("DodgeOut", 0.5f); 
+        }
+    }
+
+    void DodgeOut()
+    {
+        speed *= 0.5f;
+        isDodge = false;
+    }
+
+        //OnCollisionEneter() 로 착지 구현
+        void OnCollisionEnter(Collision collision)
+    {
+        // 태그를 활용하여 바닥에 대해서만 작동
+        if(collision.gameObject.tag == "Floor")
+        {
+            anim.SetBool("isJump", false);
+            isJump = false;
+            
+        }
+
+    }
 }
